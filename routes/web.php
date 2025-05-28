0077cef49e597553df8485c8e908568777938f83
RasmuS2024 <rasburkhanov@yandex.ru>
2025-05-28 23:19:17 +0300

    modified:   config/database.php, routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+Route::get('/check-db', function() {
+    try {
+        DB::connection()->getPdo();
+        return response()->json([
+            'status' => 'Connected successfully',
+            'db' => DB::connection()->getDatabaseName()
+        ]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+});
