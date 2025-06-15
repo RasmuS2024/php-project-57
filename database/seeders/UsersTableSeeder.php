95760729adda4835b6546eb575394a68e56221b5
RasmuS2024 <rasburkhanov@yandex.ru>
2025-06-15 23:29:19 +0300

    fixed dubles in UsersTableSeeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -8,59 +8,58 @@ use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
-    private const PASSWORD = 'password';
     public function run()
     {
         $users = [
             [
                 'name' => 'Матвей Иванович Ефремов',
                 'email' => 'efremov@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('Xk9!qP2$mL7nR5tY'),
             ],
             [
                 'name' => 'Эдуард Иванович Хохлов',
                 'email' => 'khokhlov@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('G7h#bN4@pQ1vK8wZ'),
             ],
             [
                 'name' => 'Лидия Фёдоровна Петрова',
                 'email' => 'petrova@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('T3r&cV6!sD9fX2mP'),
             ],
             [
                 'name' => 'Прохор Андреевич Жданов',
                 'email' => 'zhdanov@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('L5k@jH8#qW4eR7tU'),
             ],
             [
                 'name' => 'Ирина Андреевна Бобылёва',
                 'email' => 'bobyleva@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('B4v%nM1!kI9oL6pY'),
             ],
             [
                 'name' => 'Ираклий Александрович Евдокимов',
                 'email' => 'evdokimov@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('P8o#dF3$zX6qW9jN'),
             ],
             [
                 'name' => 'Евгения Евгеньевна Герасимова',
                 'email' => 'gerasimova@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('N2m!bV7#kL5pQ9wE'),
             ],
             [
                 'name' => 'Марина Борисовна Смирнова',
                 'email' => 'smirnova@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('R9t$yH4!uJ7iK2lO'),
             ],
             [
                 'name' => 'Альберт Максимович Власов',
                 'email' => 'vlasov@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('S5f#gH8@jK3lP6qW'),
             ],
             [
                 'name' => 'Марк Евгеньевич Аксёнов',
                 'email' => 'aksenov@example.com',
-                'password' => Hash::make(self::PASSWORD),
+                'password' => Hash::make('W6e$rT2!yU9iO4pA'),
             ],
         ];
 
